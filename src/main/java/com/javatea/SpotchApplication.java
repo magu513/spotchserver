42345cc5894f5e08ff5e978a5b22060b704a559f
Shoya Yamaguchi <kbc14a15@stu.kawahara.ac.jp>
2016-12-12 12:20:05 +0900

    create project

---
+++ src/main/java/com/javatea/SpotchApplication.java
@@ -0,0 +1,12 @@
+package com.javatea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotchApplication.class, args);
+	}
+}
