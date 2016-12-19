e74cdf0f1dbd43cce9e402d7b215888a6d99299b
Shoya Yamaguchi <kbc14a15@stu.kawahara.ac.jp>
2016-12-19 13:51:09 +0900

    Userクラスの追加

---
+++ src/main/java/com/javatea/User.java
@@ -0,0 +1,58 @@
+package com.javatea;
+
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "User")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long userId;
+
+	private String userName;
+	private String email;
+	private Date birthDate;
+	private Date registerDate;
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Date getRegisterDate() {
+		return registerDate;
+	}
+
+	public void setRegisterDate(Date registerDate) {
+		this.registerDate = registerDate;
+	}
+}
