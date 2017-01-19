1bf356168bbf3bd6b4d14f5e06ade886e8f41e9c
Shoya Yamaguchi <kbc14a15@stu.kawahara.ac.jp>
2017-01-19 12:12:55 +0900

    UserDao型のメンバ変数の追加
    Autowiredアノテーションがついているため明示的にインスタンスを生成しない

--- src/main/java/com/javatea/spotchserver/controller/UserController.java
+++ src/main/java/com/javatea/spotchserver/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.javatea.spotchserver.controller;
 
 import com.javatea.spotchserver.User;
+import com.javatea.spotchserver.db.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +12,9 @@ import java.util.Date;
 
 @Controller
 public class UserController {
+	@Autowired
+	private UserDao ud;
+
 	@RequestMapping(value = "/",method = RequestMethod.POST)
 	public void signIn(@RequestParam String username,
 					   @RequestParam String email,
