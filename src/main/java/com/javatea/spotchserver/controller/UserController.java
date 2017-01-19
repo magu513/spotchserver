65921326e34767fa458558bd081530907a7b4b14
Shoya Yamaguchi <kbc14a15@stu.kawahara.ac.jp>
2017-01-19 12:10:45 +0900

    リクエストメソッドと引数の追加

--- src/main/java/com/javatea/spotchserver/controller/UserController.java
+++ src/main/java/com/javatea/spotchserver/controller/UserController.java
@@ -1,12 +1,19 @@
 package com.javatea.spotchserver.controller;
 
+import com.javatea.spotchserver.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
 
 @Controller
 public class UserController {
-	@RequestMapping("/")
-	public void signIn() {
+	@RequestMapping(value = "/",method = RequestMethod.POST)
+	public void signIn(@RequestParam String username,
+					   @RequestParam String email,
+					   @RequestParam Date birthday) {
 
 	}
 }
