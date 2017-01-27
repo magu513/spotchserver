301d238383c89f1f89900c88dc9a98dc8456a088
Shoya Yamaguchi <kbc14a15@stu.kawahara.ac.jp>
2017-01-27 12:28:29 +0900

    日付を変換するクラスの作成
    日付<->文字列
    ArticleDao,Articleクラスなどコードの変更の必要あり。

---
+++ src/main/java/com/javatea/spotchserver/opt/DateFomatter.java
@@ -0,0 +1,32 @@
+package com.javatea.spotchserver.opt;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateFomatter {
+	private static DateTimeFormatter dtf;
+
+	public static LocalDateTime stringToDateTime(String dateStr) {
+		LocalDateTime localDateTime = LocalDateTime.parse(dateStr,dtf);
+		return localDateTime;
+	}
+
+	public static LocalDateTime stringToDateTime(String dateStr, String pattern) {
+		setPattern(pattern);
+		return stringToDateTime(dateStr);
+	}
+
+	public static void setPattern(String pattern) {
+		dtf = DateTimeFormatter.ofPattern(pattern);
+	}
+
+	public static String dateTimeToString(LocalDateTime date) {
+		return dtf.format(date);
+	}
+
+	public static String dateTimeToString(LocalDateTime date,
+								   String pattern) {
+		setPattern(pattern);
+		return dateTimeToString(date);
+	}
+}
